01983c7550ce335625ddd58892b8806eb079405a
ErnestoRe <ernestorezustaaznarez@gmail.com>
2024-11-14 08:30:28 +0100

    funciones2

--- Reto3/src/Ejercicios/funciones2.java
+++ Reto3/src/Ejercicios/funciones2.java
@@ -1,5 +1,57 @@
 package Ejercicios;
 
-public class funciones2 {
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
+public class funciones2 {
+	public static void diferenteAnio(LocalDate fecha1, LocalDate fecha2) {
+		int diasTotal = 0;
+		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+
+		
+		fecha2 = LocalDate.of(fecha2.getYear(),fecha2.getMonth(),1);
+		
+		while(fecha1.isBefore(fecha2)) {
+			int contador_dias = fecha1.lengthOfMonth();
+			fecha1 = fecha1.plusMonths(1);
+			diasTotal = diasTotal+contador_dias;
+		}
+		int mediaDias = diasTotal/meses;
+		System.out.println(mediaDias+" es la media de dias");
+		funciones2.consonantes(fecha2);
+	}
+	
+	public static void consonantes(LocalDate fecha2) {
+		String mesFecha2 = String.valueOf(fecha2.getMonth());
+		char caracter;
+		for(int i = 0; i<mesFecha2.length();i++) {
+			caracter = mesFecha2.charAt(i);
+			if(caracter!='a' && caracter!='e' && caracter!='i' && caracter!='o' && caracter!='u') {
+				if(i==0) {
+					System.out.print(caracter);
+				}else {
+					System.out.print(", "+caracter);
+				}
+				
+			}
+		}
+	}
+	
+	public static LocalDate dimeFecha(String texto, Scanner sc) {
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		do {
+			try {
+				System.out.println(texto);
+				String textoFecha = sc.nextLine();
+				LocalDate fecha = LocalDate.parse(textoFecha, formato);
+				return fecha;
+			} catch (Exception ex) {
+				System.out.println("Fecha no valida");
+			}
+		} while (true);
+	}
 }
+
--- Reto3/src/Ejercicios/reto3.java
+++ Reto3/src/Ejercicios/reto3.java
@@ -1,10 +1,21 @@
 package Ejercicios;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		LocalDate fecha1 = funciones2.dimeFecha("Introduce una fecha", sc);
+		LocalDate fecha2;
+		do {
+			fecha2 = funciones2.dimeFecha("Introduce otra fecha", sc);
+		}while(fecha2.isBefore(fecha1));
+		
+		if(fecha1.getYear()==fecha2.getYear()) {
+			
+		}
 	}
 
 }
